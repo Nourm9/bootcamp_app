e2318d0d507dd36fbe02315836e85d91a38e5ed5
Nour Mohamoud <nour.mohamoud9@gmail.com>
2023-02-26 21:33:59 +0000

    first commit

---
+++ teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '$'
+ORDER BY teacher;
+`
+
+let limit = process.argv[2] || 'JUL02' 
+let values = [limit]
+pool
+  .query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  });
